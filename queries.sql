993db825c47157c2c7e1c67cb9c4f8909348d51c
siti8825fmipa2024-web <siti8825fmipa.2024@student.uny.ac.id>
2026-05-31 18:11:12 +0700

    menampilkan data aset

--- queries.sql
+++ queries.sql
@@ -1,8 +1,3 @@
-SELECT p.nama_penyewa,
-       k.id_kamar,
-       a.nama_aset,
-       a.kondisi_aset
-FROM penyewa p
-JOIN kamar k ON p.id_kamar = k.id_kamar
-JOIN aset_inventaris a ON k.id_kamar = a.id_kamar
-WHERE p.NIK = '340**02';
+SELECT *
+FROM aset_inventaris
+ORDER BY nama_aset ASC;
