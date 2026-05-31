de89c76282d2b9550c1b1e4e716e72531600534a
siti8825fmipa2024-web <siti8825fmipa.2024@student.uny.ac.id>
2026-05-31 17:59:34 +0700

    queries riwayat penyewa keluar.sql

--- queries.sql
+++ queries.sql
@@ -1,11 +1,3 @@
-SELECT 
-  th_pendapatan.Tahun,
-  th_pendapatan.total_pendapatan,
-  th_pengeluaran.total_pengeluaran
-FROM 
-  (SELECT YEAR(Tanggal_bayar) AS Tahun, SUM(total_pembayaran) AS total_pendapatan
-  FROM transaksi GROUP BY YEAR(Tanggal_bayar)) AS th_pendapatan
-LEFT JOIN
-  (SELECT YEAR(Tanggal_catat) AS Tahun, SUM(Nominal_tagihan) AS total_pengeluaran
-  FROM pengeluaran_operasional GROUP BY YEAR(Tanggal_catat)) AS th_pengeluaran
-ON th_pendapatan.Tahun = th_pengeluaran.Tahun;
+UPDATE penyewa
+SET tanggal_keluar = '2026-05-01'
+WHERE NIK = '340**02';
