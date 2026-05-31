deebf5425d2465e547f2f102130f1f00ffda1fd7
siti8825fmipa2024-web <siti8825fmipa.2024@student.uny.ac.id>
2026-05-31 17:20:58 +0700

    queries total keuntungan.sql

--- queries.sql
+++ queries.sql
@@ -1,2 +1,5 @@
-SELECT SUM(nominal_tagihan) AS total_pengeluaran
-FROM pengeluaran_operasional;
+SELECT
+(SELECT SUM(total_pembayaran) FROM transaksi)
+-
+(SELECT SUM(nominal_tagihan) FROM pengeluaran_operasional)
+AS estimasi_keuntungan;
