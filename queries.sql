68664a9db7996449c1b507c3b3efaeedeb834c05
siti8825fmipa2024-web <siti8825fmipa.2024@student.uny.ac.id>
2026-05-31 17:41:41 +0700

    Total pendapatan dan total pengeluran per tahun

--- queries.sql
+++ queries.sql
@@ -1,4 +1,11 @@
-SELECT MONTH(Tanggal_catat) AS Bulan,
-SUM(Nominal_tagihan) AS total_pengeluaran
-FROM pengeluaran_operasional
-GROUP BY MONTH(Tanggal_catat);
+SELECT 
+  th_pendapatan.Tahun,
+  th_pendapatan.total_pendapatan,
+  th_pengeluaran.total_pengeluaran
+FROM 
+  (SELECT YEAR(Tanggal_bayar) AS Tahun, SUM(total_pembayaran) AS total_pendapatan
+  FROM transaksi GROUP BY YEAR(Tanggal_bayar)) AS th_pendapatan
+LEFT JOIN
+  (SELECT YEAR(Tanggal_catat) AS Tahun, SUM(Nominal_tagihan) AS total_pengeluaran
+  FROM pengeluaran_operasional GROUP BY YEAR(Tanggal_catat)) AS th_pengeluaran
+ON th_pendapatan.Tahun = th_pengeluaran.Tahun;
