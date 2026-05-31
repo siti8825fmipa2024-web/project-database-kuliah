5dda8d8bd71ec77005dc3e98e54785971a929441
siti8825fmipa2024-web <siti8825fmipa.2024@student.uny.ac.id>
2026-05-31 18:04:30 +0700

    informasi kamar dan aset sebelum penyewa keluar

--- queries.sql
+++ queries.sql
@@ -1,3 +1,8 @@
-UPDATE penyewa
-SET tanggal_keluar = '2026-05-01'
-WHERE NIK = '340**02';
+SELECT p.nama_penyewa,
+       k.id_kamar,
+       a.nama_aset,
+       a.kondisi_aset
+FROM penyewa p
+JOIN kamar k ON p.id_kamar = k.id_kamar
+JOIN aset_inventaris a ON k.id_kamar = a.id_kamar
+WHERE p.NIK = '340**02';
